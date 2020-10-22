402bfe46480d77a0e0b2b5730734ba35bf4fe2e7
josue <josuensumba@gmail.com>
2020-10-22 19:46:57 +0200

    first commit

---
+++ backend/src/Backend.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import main;
+
+@SpringBootApplication
+public class Backend {
+
+/*    public static void main(String... args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }*/
+
+}
