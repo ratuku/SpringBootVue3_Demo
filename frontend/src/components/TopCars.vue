12bd61852fbf32c18a853ce358c7942730a88dee
josue <josuensumba@gmail.com>
2020-10-23 09:48:40 +0200

    add 3 components

---
+++ frontend/src/components/TopCars.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Top Cars of All Time</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TopCars"
+    }
+</script>
+
+<style scoped>
+
+</style>
